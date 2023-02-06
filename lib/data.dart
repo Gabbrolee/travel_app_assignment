3c1499a6558ea79f8dc72d9601e0f8a4246312b1
Ameh Christopher <akrine2christophine@gmail.com>
2023-02-06 19:20:59 +0100

    create a dummy data for the choose category

---
+++ lib/data.dart
@@ -0,0 +1,17 @@
+import 'package:travel_app_ui_assignment/app_icons.dart';
+import 'package:travel_app_ui_assignment/model/category.dart';
+
+final baga = Categories(
+    imageUrl: bagaBeach,
+    name: 'Baga Beach',
+    address: 'Goa, India'
+);
+
+final kuta = Categories(
+    imageUrl: kutaIcon,
+    name: 'Kuta Beach',
+    address: 'Bali, Indonesia'
+);
+
+final choosenCategory = [kuta, baga, kuta, baga, kuta, baga, kuta, baga, kuta, baga];
+
