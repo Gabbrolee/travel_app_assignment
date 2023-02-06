d55ed342a4d080214741f8e480cb7480a5b78b75
Ameh Christopher <akrine2christophine@gmail.com>
2023-02-06 19:17:02 +0100

    create choose category model

---
+++ lib/model/category.dart
@@ -0,0 +1,8 @@
+class Categories {
+  final String imageUrl;
+  final String name;
+  final String address;
+  final bool isFavourite;
+
+  Categories({required this.imageUrl, required this.name, required this.address, this.isFavourite = false});
+}
