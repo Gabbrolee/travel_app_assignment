e9a56841c38c352e564e5f7b3a915fb2bfc783bb
Ameh Christopher <akrine2christophine@gmail.com>
2023-02-06 19:18:20 +0100

    add icon path to the app_icon

---
+++ lib/app_icons.dart
@@ -0,0 +1,8 @@
+
+
+const lamp = 'images/lamp.svg';
+const beachTree = 'images/tree_icon.png';
+const mountainIcon = 'images/mountain.png';
+const bagaBeach = 'images/bagaBaga.png';
+const kutaIcon = 'images/kuta_kuta.png';
+const locationIcon = 'images/location.png';
