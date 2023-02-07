c429184d0c7c4ee3c1131c45e072aa1ebf5d1331
Ameh Christopher <akrine2christophine@gmail.com>
2023-02-07 14:11:19 +0100

    update images path

--- lib/app_icons.dart
+++ lib/app_icons.dart
@@ -6,3 +6,5 @@ const mountainIcon = 'images/mountain.png';
 const bagaBeach = 'images/bagaBaga.png';
 const kutaIcon = 'images/kuta_kuta.png';
 const locationIcon = 'images/location.png';
+const kutaResortImage = 'images/kuta_resort.png';
+const bugaResortImage = 'images/baga_resort.png';
