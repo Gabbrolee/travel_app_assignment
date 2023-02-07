8f0cd3cbd439a2b5cb1100f387fe6a1d339a17cd
Ameh Christopher <akrine2christophine@gmail.com>
2023-02-07 14:13:29 +0100

    add popular packages model

--- lib/data.dart
+++ lib/data.dart
@@ -1,5 +1,6 @@
 import 'package:travel_app_ui_assignment/app_icons.dart';
 import 'package:travel_app_ui_assignment/model/category.dart';
+import 'package:travel_app_ui_assignment/model/popular_packages.dart';
 
 final baga = Categories(
     imageUrl: bagaBeach,
@@ -14,4 +15,25 @@ final kuta = Categories(
 );
 
 final choosenCategory = [kuta, baga, kuta, baga, kuta, baga, kuta, baga, kuta, baga];
+
+
+final kutaResort = PopularPackages(
+   imageUrl: kutaResortImage,
+   name: 'Kuta Resort',
+   price: '20000',
+   description: 'A resort is a place used for\n vacation, relaxation or as a day...',
+   rating: 4.8,
+
+);
+
+final bugaResort = PopularPackages(
+  imageUrl: bugaResortImage,
+  name: 'Baga Beach',
+  price: '15000',
+  description: 'A resort is a place used for\n vacation, relaxation or as a day...',
+  rating: 4.8,
+
+);
+
+List<PopularPackages> choosenPackages = [kutaResort, bugaResort, kutaResort];
 
