71f3e1ff148da7188b9252a9eb1958b30a6574e5
Ameh Christopher <akrine2christophine@gmail.com>
2023-02-07 14:20:51 +0100

    create a model for popular packages

---
+++ lib/model/popular_packages.dart
@@ -0,0 +1,18 @@
+class PopularPackages {
+
+  final String imageUrl;
+  final String name;
+  final String price;
+  final String description;
+  final double rating;
+
+
+  PopularPackages(
+      {
+      required this.imageUrl,
+      required this.name,
+      required this.price,
+      required this.description,
+      required this.rating,
+      });
+}
